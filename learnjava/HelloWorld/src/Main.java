df333b59ca3b47304da215eda56cf9c12f70a4d2
yuanxiaowen986 <18770088220@163.com>
2019-08-09 16:05:42 +0800

    modify format

--- learnjava/HelloWorld/src/Main.java
+++ learnjava/HelloWorld/src/Main.java
@@ -6,12 +6,12 @@
  * @author yuanxiaowen
  */
 public class Main {
-    public static void main(String[] args) {
-        int n = 100;
-        // TODO: sum = 1 + 2 + ... + n
-        int sum = ((1+n)*n)/2;
+	public static void main(String[] args) {
+		int n = 100;
+		// TODO: sum = 1 + 2 + ... + n
+		int sum = ((1 + n) * n) / 2;
 
-        System.out.println(sum);
-        System.out.println(sum == 5050 ? "测试通过" : "测试失败");
-    }
+		System.out.println(sum);
+		System.out.println(sum == 5050 ? "测试通过" : "测试失败");
+	}
 }
