d4f6b8d0c4de45f3b58e4cf8dacc44d3d9f38326
yuanxiaowen986 <18770088220@163.com>
2019-08-16 09:33:46 +0800

    add java 2.1.2

--- learnjava/HelloWorld/src/FunctionExample.java
+++ learnjava/HelloWorld/src/FunctionExample.java
@@ -9,24 +9,3 @@ public class FunctionExample {
 		System.out.println(ming.getAge());
 	}
 }
-
-class Person {
-	private String name;
-	private int age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return this.age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-}
