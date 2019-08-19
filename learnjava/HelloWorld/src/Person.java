bab0e162e688cb719d3d0629e767285ea2f75d5f
yuanxiaowen986 <18770088220@163.com>
2019-08-19 14:05:12 +0800

    add java 2.1.8

--- learnjava/HelloWorld/src/Person.java
+++ learnjava/HelloWorld/src/Person.java
@@ -1,14 +1,22 @@
 /**
  * 给Person增加构造方法; 给Person增加重载方法setName(String, String)
  * 
- * @author yuanxiaowen
+ * 给Person类增加一个静态字段count和静态方法getCount，统计实例的个数
+ * 
+ * @author administrator
  */
 public class Person {
 	private String name;
 	private int age;
+	public static int count;
 
 	public Person() {
 
+	}
+
+	public Person(String name) {
+		this.name = name;
+		count++;
 	}
 
 	public Person(String name, int age) {
@@ -34,5 +42,9 @@ public class Person {
 
 	public void setName(String prename, String lastname) {
 		this.name = prename + " " + lastname;
+	}
+
+	public static int getCount() {
+		return count;
 	}
 }
