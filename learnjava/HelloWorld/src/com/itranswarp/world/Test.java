dc89537018c181f4af6d1375837fc77f1d33e684
yuanxiaowen986 <18770088220@163.com>
2019-09-26 21:16:51 +0800

    modify java 8.3

--- learnjava/HelloWorld/src/com/itranswarp/world/Test.java
+++ learnjava/HelloWorld/src/com/itranswarp/world/Test.java
@@ -8,18 +8,12 @@ package com.itranswarp.world;
  */
 public class Test {
 	public static void main(String[] args) {
-		String s = "190";
-		int i = (int) (s.charAt(2));
-		Integer it = Integer.valueOf(s);
-		int i2 = it.intValue();
-		char i3 = s.charAt(2);
-		Integer it2 = Integer.valueOf(i3);
-		int i4 = it2.intValue();
-		System.out.println(i);
-		System.out.println(it);
-		System.out.println(i2);
-		System.out.println(it2);
-		System.out.println(i4);
+		int n = 15 + (15 * 16);
+		double n2 = 1 * Math.pow(2, 0) + 1 * Math.pow(2, 1) + 1
+				* Math.pow(2, 2) + 1 * Math.pow(2, 3) + 1 * Math.pow(2, 4) + 1
+				* Math.pow(2, 5) + 1 * Math.pow(2, 6) + 1 * Math.pow(2, 7);
+		System.out.println(n);
+		System.out.println(n2);
 	}
 
 }
