a3494d4380a6c831ae78980e29871cf44eeebd3b
yuanxiaowen986 <18770088220@163.com>
2019-10-11 12:23:51 +0800

    add java 9.2 Date and Calendar

--- learnjava/HelloWorld/src/com/itranswarp/world/Test.java
+++ learnjava/HelloWorld/src/com/itranswarp/world/Test.java
@@ -1,5 +1,10 @@
 package com.itranswarp.world;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * 测试专用
  * 
@@ -8,12 +13,20 @@ package com.itranswarp.world;
  */
 public class Test {
 	public static void main(String[] args) {
-		int n = 15 + (15 * 16);
-		double n2 = 1 * Math.pow(2, 0) + 1 * Math.pow(2, 1) + 1
-				* Math.pow(2, 2) + 1 * Math.pow(2, 3) + 1 * Math.pow(2, 4) + 1
-				* Math.pow(2, 5) + 1 * Math.pow(2, 6) + 1 * Math.pow(2, 7);
-		System.out.println(n);
-		System.out.println(n2);
+		// 当前时间:
+		Calendar c = Calendar.getInstance();
+		// 清除所有:
+		c.clear();
+		// 设置年月日时分秒:
+		c.set(2019, 10 /* 11月 */, 20, 8, 15, 0);
+		// 加5天并减去2小时:
+		c.add(Calendar.DAY_OF_MONTH, 5);
+		c.add(Calendar.HOUR_OF_DAY, -2);
+		// 显示时间:
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date d = c.getTime();
+		System.out.println(sdf.format(d));
+		// 2019-11-25 6:15:00
 	}
 
 }
