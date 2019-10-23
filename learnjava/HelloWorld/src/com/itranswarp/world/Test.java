43491f8847f6bbf7cf3586c4d936888febc45d83
yuanxiaowen986 <18770088220@163.com>
2019-10-23 20:49:05 +0800

    add java 11.1

--- learnjava/HelloWorld/src/com/itranswarp/world/Test.java
+++ learnjava/HelloWorld/src/com/itranswarp/world/Test.java
@@ -1,9 +1,5 @@
 package com.itranswarp.world;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 /**
  * 测试专用
@@ -13,20 +9,9 @@ import java.util.TimeZone;
  */
 public class Test {
 	public static void main(String[] args) {
-		// 当前时间:
-		Calendar c = Calendar.getInstance();
-		// 清除所有:
-		c.clear();
-		// 设置年月日时分秒:
-		c.set(2019, 10 /* 11月 */, 20, 8, 15, 0);
-		// 加5天并减去2小时:
-		c.add(Calendar.DAY_OF_MONTH, 5);
-		c.add(Calendar.HOUR_OF_DAY, -2);
-		// 显示时间:
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date d = c.getTime();
-		System.out.println(sdf.format(d));
-		// 2019-11-25 6:15:00
+		String regex = "20\\d\\d";
+		System.out.println("2019".matches(regex));
+		System.out.println("2109".matches(regex));
 	}
 
 }
